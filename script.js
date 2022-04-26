17b4432505e44fc731e82df6a6554b94624fdd70
Tim <98896176+twistedstached@users.noreply.github.com>
2022-04-26 06:35:26 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function logout(element) {
+    element.innerText = "Logout"
+}
+
+function likeAdd(element) {
+    element.innerText++;
+    alert('Ninja was liked')
+}
+
+function remove(element) {
+    element.remove();
+}
